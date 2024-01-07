15dd422fc88fb87faf597bb1fa772e3f2e155980
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-06 16:01:47 -0800

    get routes

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const generateId = require('generate-unique-id');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require('./db/db.json');
+const { notes } = require('./Develop/db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
 
