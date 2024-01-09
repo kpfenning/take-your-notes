bda0aa788771986308b206991fd6c3a79e7de6c2
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-09 15:55:16 -0800

    finally getting routes to work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const randomUuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,14 +15,14 @@ app.use(express.static('public'));
 let notes = [];
 
 app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req,res) => {
-    fs.readFile(path.join(__dirname, 'Develop/db/db.json'), (err,data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err,data) => {
         if (err) {
             console.error(err);
-            return res.status(500).json({error});
+            return res.status(500).json({ message: 'error'});
         }
         const notes = JSON.parse(data);
         res.json(notes);
@@ -31,29 +31,61 @@ app.get('/api/notes', (req,res) => {
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    newNote.id = randomUuid();
+    newNote.id = uuidv4();
 
-    fs.readFile(path.join(__dirname, 'Develop/db/db.json'), (err,data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'),'utf8', (err,data) => {
         if (err) {
             console.error(err);
-            return res.status(500).json({error});
+            return res.status(500).json({ message: 'error'});
         }
         const notes = JSON.parse(data);
         notes.push(newNote);
-        fs.writeFile(path.join(__dirname, 'Develop/db/db.json'), JSON.stringify(notes), (err) => {
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), (err) => {
             if(err) {
                 console.error(err);
-                return res.status(500).json({error});
+                return res.status(500).json({ message: 'error'});
             }
             res.json(newNote);
         })
     });
 });
 
+app.delete('/api/notes/:id', (req,res) => {
+    const noteId = req.params.id;
+
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err,data) => {
+        if(err) {
+            console.error(err);
+            return res.status(500).json({ message: 'error'});
+        }
+        const notes = JSON.parse(data);
+        const index = notes.findIndex((note) => note.id === noteId);
+
+        if (index !== -1) {
+            const deleteNote = notes.splice(index,1) [0];
+
+            fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), (err) => {
+                if(err) {
+                    console.error(err);
+                    return res.status(500).json({ message: 'error'});
+                }
+                res.json(deleteNote);
+            }
+            );   
+    } else {
+        res.status(404).json({ message: "No note found"});
+    }
+    });
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
 
 
 
