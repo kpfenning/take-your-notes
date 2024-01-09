0bb28f6d1cc8f86003b51080b91f27ccd4c8437e
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-09 14:47:00 -0800

    starting fresh

--- server.js
+++ server.js
@@ -6,25 +6,16 @@ const generateId = require('generate-unique-id');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require('./Develop/db/db.json');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
+let notes = [];
 
 
-app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
-});
+
+
+
+
