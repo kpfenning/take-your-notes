f482821ca8b06d871f5e835671f381481173df14
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-09 14:56:44 -0800

    got notes route done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const generateId = require('generate-unique-id');
+const uuid = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +13,26 @@ app.use(express.json());
 app.use(express.static('public')); 
 
 let notes = [];
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+});
+
+app.get('/api/notes', (req,res) => {
+    fs.readFile(path.join(__dirname, 'Develop/db/db.json'), (err,data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({error});
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
 
